45b1b7d50390c860b8d6699c7d9841c5b83a334c
hide2222 <abe.edipa@gmail.com>
2020-08-26 19:06:28 +0900

    with_options実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length:{minimum: 6}, format: { with: /\A[a-zA-Z0-9]+\z/}
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday_id,presence: true
-
+   with_options presence: true do
+    validates :nickname
+    validates :birthday_id
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length:{minimum: 6},format: { with: /\A[a-zA-Z0-9]+\z/}
+        
+   with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :family_name
+      validates :first_name
+    end
+        
+   with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+   end
 end
