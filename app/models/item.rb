50a0c13191536c53b3e46e49e6f0f23612d30669
hide2222 <abe.edipa@gmail.com>
2020-08-27 16:22:26 +0900

    ActiveStrageの実装開始

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :categoly
+  belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :ship_fee
   belongs_to_active_hash :ship_schedule
