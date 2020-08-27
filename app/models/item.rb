359e0bd969e9c69582c5e4b15cc8839b840fda1d
hide2222 <abe.edipa@gmail.com>
2020-08-27 13:55:05 +0900

    ActiveHash各モデル記述完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,29 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  validates :prefecture, presence: true
-  validates :prefecture_id, numericality: { other_than: 0 }
-end
+  belongs_to_active_hash :categoly
+  belongs_to_active_hash :status
+  belongs_to_active_hash :ship_fee
+  belongs_to_active_hash :ship_schedule
+
+  
+
+  with_options presence: true do
+  validates :prefecture
+  validates :category
+  validates :status
+  validates :ship_fee
+  validates :ship_schedule
+  validates :price
+  validates :name
+  validates :item_detail
+  end
+      
+  with_options numericality: { other_than: 0 } do
+  validates :prefecture_id
+  validates :category_id
+  validates :status_id
+  validates :ship_fee_id
+  validates :ship_schedule_id
+  end
+ end
