29274bc7784691ce7981c2c09ab087ca038d50d3
hide2222 <abe.edipa@gmail.com>
2020-08-27 11:05:25 +0900

    prefectureモデル設定完了（ActiveHash)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  validates :prefecture, presence: true
+  validates :prefecture_id, numericality: { other_than: 0 }
 end
