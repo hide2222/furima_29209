cbf9bdf4b808ce4ff70fd119f50a018444be252d
hide2222 <abe.edipa@gmail.com>
2020-08-28 16:36:20 +0900

    計算式javascript完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,8 +21,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
-
   
   def item_params
     params.require(:item).permit(:user,:name,:item_detail,:price,:category_id,:status_id,:ship_fee_id,:prefecture_id,:ship_schedule_id, :image).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
+
+  #金額を取得したいのでqueryパラメーターを使用
+get 'items', to: 'items#calculation'
 end
