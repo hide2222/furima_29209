412b6f29b9fd1309ee3f4441d6b38a7482ac29e5
hide2222 <abe.edipa@gmail.com>
2020-09-06 14:52:28 +0900

    新規登録パスワード英数混合修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :password, length:{minimum: 6},format: { with: /\A[a-zA-Z0-9]+\z/}
+    validates :password, length:{minimum: 6},format: { with:  /\A(?=.*?[a-z])[a-z\d]{6,32}+\z/}
         
    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
       validates :family_name
